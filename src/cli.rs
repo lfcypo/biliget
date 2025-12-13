5d75de00c014322f80d941107b15ac2542d605fd
lfcypo <lfcypo@gmail.com>
2025-12-13 13:51:07 +0800

    feat: 显示版本号

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 
 #[derive(Parser)]
-#[command(about = "简单的B站视频下载工具 可以免登录下载B站高清视频", long_about = None)]
+#[command(version, about = "简单的B站视频下载工具 可以免登录下载B站高清视频", long_about = None)]
 pub struct Cli {
     /// Bilibili 视频地址
     pub url: String,
